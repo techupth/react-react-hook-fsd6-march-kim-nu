6f6c5248e0f6cf08d7214903fbecaefe120bcf44
Athikhom_ra <athikhom_ra@outlook.com>
2024-06-13 13:24:30 +0700

    Assignment 'react custom hook' NARu

---
+++ client/src/hooks/useBlogPosts.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { useState } from "react";
+function useBlogPosts() {
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios("http://localhost:4000/posts");
+      setPosts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+  return [posts, isError, isLoading, getPosts];
+}
+export default useBlogPosts;
